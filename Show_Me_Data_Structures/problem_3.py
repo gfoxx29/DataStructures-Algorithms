f2ba6f6efd797dbc63bfbfd36b98edb2a649a451
gfoxx29 <nalbert9@hotmail.fr>
2019-07-29 15:03:16 +0200

    Resolve problem 3

--- Show_Me_Data_Structures/problem_3.py
+++ Show_Me_Data_Structures/problem_3.py
@@ -1,10 +1,35 @@
 import sys
 
 def huffman_encoding(data):
-    pass
+    huffman = {}
+    tree = {}
+    temp = '1'
+    for char in data:
+        huffman[char] = huffman.get(char, 0) + 1
+    for num in sorted(huffman.items(), key = lambda x: x[1]):
+        tree[num[0]] = temp
+        temp = '0' + temp
 
-def huffman_decoding(data,tree):
-    pass
+    encoded_data = ''
+    for d in data:
+        encoded_data += tree[d]
+    return encoded_data, tree
+
+def huffman_decoding(data, tree):
+    huffman = {}
+    for char in tree:
+        huffman[tree[char]] = char
+
+    temp = ''
+    decoded_data = ''
+    for d in data:
+        if d == '1':
+            decoded_data += huffman[temp + d]
+            temp = ''
+        else:
+            temp += d
+    return decoded_data
+
 
 if __name__ == "__main__":
     codes = {}
